2698465a928bde029456c5bcc6707d7e737122cc
Rose Khaldeeva <khalrose@mail.ru>
2022-10-11 17:10:11 +0600

    Replace IntroductionViiewController

---
+++ PersonalityQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Rose on 02.10.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
+
