29b45a01410d3c5f413a226dce1a6999e01a8b57
Rose Khaldeeva <khalrose@mail.ru>
2022-10-12 21:01:25 +0600

    Setup QuestionViewController

--- PersonalityQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalityQuiz/ViewControllers/QuestionViewController.swift
@@ -12,7 +12,16 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var questionProgressView: UIProgressView!
     
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
     
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabel: [UILabel]!
+    @IBOutlet var multipleSwitch: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet weak var rangedSlider: UISlider!
     
     private let question = Question.getQuestion()
     
@@ -20,9 +29,20 @@ class QuestionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        
     }
+    
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    
+    
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+    
     
 
     /*
